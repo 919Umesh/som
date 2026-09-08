4613be6aac8d30ac10b7c9550745334958a42554
Umesh <globaltechumesh11@gmail.com>
2026-09-08 10:58:43 +0545

    Start: Project Created

---
+++ app/src/main/java/com/example/som/ui/screens/ServiceListScreen.kt
@@ -0,0 +1,21 @@
+package com.example.som.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ServiceListScreen() {
+    Column {
+        Text(
+            text = "Services"
+        )
+
+        Button(
+            onClick = {}
+        ) {
+            Text("Book Service")
+        }
+    }
+}
